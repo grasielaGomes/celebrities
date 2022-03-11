4ff09b184496c8fed46dc684921fdba5d59f9d3e
Grasiela Gomes <produtoradama@gmail.com>
2022-03-11 10:13:15 -0300

    test: added typography components

---
+++ src/components/Typography/__tests__/Typography.spec.tsx
@@ -0,0 +1,50 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Label, Caption, Body, Heading5, Heading4, Heading6 } from '../Typography';
+
+describe('Typography', () => {
+  const text = 'Text';
+  const color = 'primary';
+  const textDecoration = 'underline';
+
+  test('Label should render with props values', () => {
+    render(<Label text={text} color={color} textDecoration={textDecoration} />);
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+  });
+
+  test('Caption should render with props values', () => {
+    render(<Caption text={text} color={color} textDecoration={textDecoration} />);
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+  });
+
+  test('Body should render with props values', () => {
+    render(<Body text={text} color={color} textDecoration={textDecoration} />);
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+  });
+  test('Heading4 should render with props values', () => {
+    render(
+      <Heading4 text={text} color={color} textDecoration={textDecoration} />,
+    );
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+  });
+
+  test('Heading5 should render with props values', () => {
+    render(
+      <Heading5 text={text} color={color} textDecoration={textDecoration} />,
+    );
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+  });
+
+  test('Heading6 should render with props values', () => {
+    render(
+      <Heading6 text={text} color={color} textDecoration={textDecoration} />,
+    );
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+  });
+});
