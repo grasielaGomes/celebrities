7d457ab4f31cd54fb38b157892ccd647e9059b3e
Grasiela Gomes <produtoradama@gmail.com>
2022-03-12 07:14:16 -0300

    feat: added containers components

---
+++ src/components/Containers/Containers.tsx
@@ -0,0 +1,63 @@
+import { IGrid, ICard, IFlex, IContainers } from './Interfaces';
+
+export const PageTemplate = ({ children }: IContainers) => {
+  return (
+    <main className="slate-100">
+      <section className="w-full min-h-screen mx-auto bg-white 2xl:w-3/4">
+        {children}
+      </section>
+    </main>
+  );
+};
+
+export const Grid = ({
+  gridKey,
+  children,
+  smColumns = '1',
+  mdColumns = '1',
+  lgColumns = '1',
+  xlColumns = '1',
+  gap = '4',
+}: IGrid) => {
+  const style = `grid grid-cols-1 sm:grid-cols-${smColumns} md:grid-cols-${mdColumns} lg:grid-cols-${lgColumns} xl:grid-cols-${xlColumns} gap-${gap}`;
+  return (
+    <section key={gridKey} className={style}>
+      {children}
+    </section>
+  );
+};
+
+export const Card = ({
+  cardKey,
+  children,
+  color = 'transparent',
+  borderRadius = 'rounded',
+  containerDecoration = '',
+  isOutlined = true,
+  paddingX = '5',
+  paddingY = '5',
+}: ICard) => {
+  const style = `py-${paddingY} px-${paddingX} rounded-${borderRadius} ${containerDecoration} ${
+    isOutlined ? `border border-${color}` : `bg-${color}`
+  }`;
+  return (
+    <section key={cardKey} className={style}>
+      {children}
+    </section>
+  );
+};
+
+export const Flex = ({
+  flexKey,
+  children,
+  gap = '4',
+  alignItems = 'center',
+  justifyContent = 'between',
+}: IFlex) => {
+  const style = `flex w-full gap-${gap} items-${alignItems} justify-${justifyContent}`;
+  return (
+    <section key={flexKey} className={style}>
+      {children}
+    </section>
+  );
+};
---
+++ src/components/Containers/Interfaces.ts
@@ -0,0 +1,29 @@
+export interface IContainers {
+  children: JSX.Element | JSX.Element[];
+}
+
+export interface IGrid extends IContainers {
+  gridKey?: string;
+  smColumns?: string;
+  mdColumns?: string;
+  lgColumns?: string;
+  xlColumns?: string;
+  gap?: string;
+}
+
+export interface ICard extends IContainers {
+  cardKey?: string;
+  color?: string;
+  borderRadius?: string;
+  containerDecoration?: string;
+  isOutlined?: boolean;
+  paddingX?: string;
+  paddingY?: string;
+}
+
+export interface IFlex extends IContainers {
+  flexKey?: string;
+  gap?: string;
+  alignItems?: string;
+  justifyContent?: string;
+}
