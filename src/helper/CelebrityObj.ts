cd362962245772c3d93831bef8c31f56f7645a01
Grasiela Gomes <produtoradama@gmail.com>
2022-03-14 13:47:09 -0300

    feat: added celebrities object

---
+++ src/helper/CelebrityObj.ts
@@ -0,0 +1,56 @@
+export const CelebryObj = [
+  {
+    name: "chuck-norris",
+  },
+  {
+    name: "tom-hanks",
+  },
+  {
+    name: "cate-blanchett",
+  },
+  {
+    name: "johnny-depp",
+  },
+  {
+    name: "kate-winslet",
+  },
+  {
+    name: "leonardo-dicaprio",
+  },
+  {
+    name: "elijah-wood",
+  },
+  {
+    name: "ian-mckellen",
+  },
+  {
+    name: "patrick-stewart",
+  },
+  {
+    name: "harrison-ford",
+  },
+  {
+    name: "natalie-portman",
+  },
+  {
+    name: "morgan-freeman",
+  },
+  {
+    name: "whoopi-goldberg",
+  },
+  {
+    name: "anthony-hopkins",
+  },
+  {
+    name: "jim-carrey",
+  },
+  {
+    name: "frances-mcdormand",
+  },
+  {
+    name: "mel-gibson",
+  },
+  {
+    name: "tom-cruise",
+  },
+];
