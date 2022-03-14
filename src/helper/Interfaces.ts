08d453189ca0438a44d76788dc06e3dddd63e347
Grasiela Gomes <produtoradama@gmail.com>
2022-03-14 17:08:16 -0300

    feat: added api response interface

---
+++ src/helper/Interfaces.ts
@@ -0,0 +1,9 @@
+export interface ICelebrityApi {
+    name: string,
+    nationality: string,
+    occupation: string[],
+    height: number,
+    birthdy: string,
+    age: number,
+    is_alive: boolean,
+}
