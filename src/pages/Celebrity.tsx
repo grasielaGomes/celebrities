c8d73805593e6a83b517ba1abab43c9baa270f73
Grasiela Gomes <produtoradama@gmail.com>
2022-03-14 19:00:50 -0300

    feat: added logic into Celebrity page

--- src/pages/Celebrity.tsx
+++ src/pages/Celebrity.tsx
@@ -16,13 +16,24 @@ import {
   ReturnIcon,
 } from "components/Images/Images";
 import { Heading1, Heading6 } from "components/Typography/Typography";
+import { ICelebrityApi } from "helper/Interfaces";
 import { useCelebrities } from "hooks/useCelebrities";
 
 export const Celebrity = () => {
-  const { navigate } = useCelebrities();
+  const { navigate, CelebrityObj, baseUrl, celebrity } = useCelebrities();
+  const filteredCelebrity = CelebrityObj.find((obj) => obj.name === celebrity);
+  const {
+    name,
+    nationality,
+    occupation,
+    height,
+    birthday,
+    age,
+    is_alive: isAlive,
+  } = filteredCelebrity as ICelebrityApi;
   return (
     <PageTemplate maxWidth="3/4">
-      <div className="h-full my-auto">
+      <div className="m-4">
         <TextButton
           handleClick={() => navigate("/")}
           child={
@@ -38,36 +49,44 @@ export const Celebrity = () => {
             </>
           }
         />
-        <Grid smColumns="2" mdColumns="2" lgColumns="2" xlColumns="2">
-          <Avatar imgUrl="https://github.com/grasielaGomes/images/blob/main/anthony-hopkins.jpg?raw=true" />
-          <Card paddingX="10">
-            <Heading1 text="Anthony Hopkins" color="primary" />
-            <Heading6 text="Age: 52" color="white" />
-            <hr className="my-5 border-gray-500" />
-            <CelebrityDetail
-              icon={<NationalityIcon />}
-              title="Nacionality"
-              text="US"
-            />
-            <CelebrityDetail
-              icon={<OcuppationIcon />}
-              title="Occupation"
-              text="actor"
-            />
-            <CelebrityDetail icon={<HeightIcon />} title="Height" text="1.74" />
-            <CelebrityDetail
-              icon={<BirthdayIcon />}
-              title="Birthday"
-              text="1969-05-14"
-            />
-            <CelebrityDetail
-              icon={<CheckPersonIcon />}
-              title="Is alive?"
-              text="yes"
-            />
-          </Card>
-        </Grid>
       </div>
+      <Grid smColumns="2" mdColumns="2" lgColumns="2" xlColumns="2">
+        <Avatar imgUrl={`${baseUrl}/${name}.jpg`} />
+        <Card paddingX="10">
+          <Heading1
+            text={name.split("-").join(" ")}
+            color="primary"
+            textDecoration="capitalize"
+          />
+          <Heading6 text={`Age ${age}`} color="white" />
+          <hr className="my-5 border-gray-500" />
+          <CelebrityDetail
+            icon={<NationalityIcon />}
+            title="Nacionality"
+            text={nationality || ""}
+          />
+          <CelebrityDetail
+            icon={<OcuppationIcon />}
+            title="Occupation"
+            text={occupation || ""}
+          />
+          <CelebrityDetail
+            icon={<HeightIcon />}
+            title="Height"
+            text={String(height) || ""}
+          />
+          <CelebrityDetail
+            icon={<BirthdayIcon />}
+            title="Birthday"
+            text={birthday || ""}
+          />
+          <CelebrityDetail
+            icon={<CheckPersonIcon />}
+            title="Is alive?"
+            text={isAlive ? "Yes" : "No"}
+          />
+        </Card>
+      </Grid>
     </PageTemplate>
   );
 };
