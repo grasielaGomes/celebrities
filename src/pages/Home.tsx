3f94f7d55d8b9976dd09c7fe437dfae1caa3cfb0
Grasiela Gomes <produtoradama@gmail.com>
2022-03-14 13:48:33 -0300

    feat: added home page celebrities list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,33 @@
-import { ImageButton } from "components/Buttons/Buttons"
-import { Grid, PageTemplate } from "components/Containers/Containers"
+import { ImageButton } from "components/Buttons/Buttons";
+import { Grid, PageTemplate } from "components/Containers/Containers";
+import { Heading1 } from "components/Typography/Typography";
+import { CelebryObj } from "helper/CelebrityObj";
+import { Fragment } from "react";
 
 export const Home = () => {
-  return <PageTemplate>
-    <Grid>
-      <ImageButton imgUrl="" altText="" />
-    </Grid>
-  </PageTemplate>
-}
+  const baseUrl = "https://raw.githubusercontent.com/grasielaGomes/images/main";
+  return (
+    <PageTemplate>
+      <Heading1
+        text="Meet the Celebrities"
+        color="white"
+        textDecoration="py-10 tracking-wide pl-10"
+      />
+      <Grid mdColumns="2" lgColumns="3" xlColumns="6" gap="0">
+        {CelebryObj.map(({ name }) => {
+          return (
+            <Fragment key={name}>
+              <ImageButton
+                width="full"
+                imgUrl={`${baseUrl}/${name}.jpg`}
+                altText={`image of ${name}`}
+                filter="grayscale"
+                hoverColor="grayscale-0 bg-primary"
+              />
+            </Fragment>
+          );
+        })}
+      </Grid>
+    </PageTemplate>
+  );
+};
