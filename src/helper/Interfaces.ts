b9a615f220ccf916fdfa6c1df3162a177dadb58a
Grasiela Gomes <produtoradama@gmail.com>
2022-03-14 18:58:58 -0300

    refactor: added undefined into ICelebrityApi interface

--- src/helper/Interfaces.ts
+++ src/helper/Interfaces.ts
@@ -1,9 +1,9 @@
 export interface ICelebrityApi {
     name: string,
-    nationality: string,
-    occupation: string[],
-    height: number,
-    birthdy: string,
-    age: number,
-    is_alive: boolean,
+    nationality?: string,
+    occupation?: string,
+    height?: number,
+    birthday?: string,
+    age?: number,
+    is_alive?: boolean,
 }
